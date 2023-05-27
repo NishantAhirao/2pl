458007087094d2f3ffa953e430a4d8f2b7218726
NishanAhirao <ahiraonishant@gmail.com>
2023-05-27 06:39:44 +0530

    L

--- As10 MD5/MD5.java
+++ As10 MD5/MD5.java
@@ -1,33 +1,45 @@
 
 public class MD5 {
     private static final int INIT_A = 0x67452301;
-    private static final int INIT_B = (int) 0xEFCDAB89L;
+    private static final int INIT_B = (int) 0xEFCDAB89L;   //4 chaining variables A B C D
     private static final int INIT_C = (int) 0x98BADCFEL;
     private static final int INIT_D = 0x10325476;
+    
     private static final int[] SHIFT_AMTS = {
             7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21
     };
+    
     private static final int[] TABLE_T = new int[64];
+    
     static {
         for (int i = 0; i < 64; i++)
             TABLE_T[i] = (int)(long)((1L << 32)* Math.abs(Math.sin(i + 1)));
     }
+   
     public static byte[] computeMD5(byte[] message) {
         int messageLenBytes = message.length;
-        int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
-        int totalLen = numBlocks << 6;
+        
+        int numBlocks = ((messageLenBytes + 8) >>> 6) + 1; //divide by 64
+        
+        int totalLen = numBlocks << 6; //multiply by 64
+        
         byte[] paddingBytes = new byte[totalLen - messageLenBytes];
         paddingBytes[0] = (byte) 0x80;
-        long messageLenBits = (long) messageLenBytes << 3;
+        
+        long messageLenBits = (long) messageLenBytes << 3; //multiply by 8
+        
         for (int i = 0; i < 8; i++) {
             paddingBytes[paddingBytes.length - 8 + i] = (byte) messageLenBits;
             messageLenBits >>>= 8;
         }
+       
         int a = INIT_A;
         int b = INIT_B;
         int c = INIT_C;
         int d = INIT_D;
+        
         int[] buffer = new int[16];
+        
         for (int i = 0; i < numBlocks; i++) {
             int index = i << 6;
             for (int j = 0; j < 64; j++, index++)
@@ -37,10 +49,12 @@ public class MD5 {
             int originalB = b;
             int originalC = c;
             int originalD = d;
+            
             for (int j = 0; j < 64; j++) {
                 int div16 = j >>> 4;
                 int f = 0;
                 int bufferIndex = j;
+               
                 switch (div16) {
                     case 0:
                         f = (b & c) | (~b & d);
@@ -58,6 +72,7 @@ public class MD5 {
                         bufferIndex = (bufferIndex * 7) & 0x0F;
                         break;
                 }
+                
                 int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] + TABLE_T[j],
                         SHIFT_AMTS[(div16 << 2) | (j & 3)]);
                 a = d;
@@ -70,17 +85,22 @@ public class MD5 {
             c += originalC;
             d += originalD;
         }
+        
         byte[] md5 = new byte[16];
         int count = 0;
+        
         for (int i = 0; i < 4; i++) {
             int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
             for (int j = 0; j < 4; j++) {
                 md5[count++] = (byte) n;
                 n >>>= 8;
             }
+        
         }
         return md5;
     }
+   
+   
     public static String toHexString(byte[] b) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < b.length; i++) {
@@ -88,6 +108,9 @@ public class MD5 {
         }
         return sb.toString();
     }
+   
+   
+   
     public static void main(String[] args) {
         String[] testStrings = {
                 "ComputerDepartment"
